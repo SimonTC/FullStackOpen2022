53c29daa0efe92ba92b5e44f9a6964837620b958
Simon Clement <simon@simontc.com>
2022-04-06 19:00:15 +0300

    course info, step 5 - use single object

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,9 +1,10 @@
 function Header(props) {
-  return <h1>{props.course}</h1>;
+  return <h1>{props.course.name}</h1>;
 }
 
 function Total(props) {
-  return <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
+  const parts = props.course.parts
+  return <p>Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>
 }
 
 function Part(props) {
@@ -11,37 +12,40 @@ function Part(props) {
 }
 
 function Content(props) {
+  const parts = props.course.parts;
   return (
     <>
-      <Part name={props.parts[0].name} numExercises={props.parts[0].exercises}/>
-      <Part name={props.parts[1].name} numExercises={props.parts[1].exercises}/>
-      <Part name={props.parts[2].name} numExercises={props.parts[2].exercises}/>
+      <Part name={parts[0].name} numExercises={parts[0].exercises}/>
+      <Part name={parts[1].name} numExercises={parts[1].exercises}/>
+      <Part name={parts[2].name} numExercises={parts[2].exercises}/>
     </>
   )
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <>
       <Header course={course}/>
-      <Content parts={parts}/>
-      <Total parts={parts}/>
+      <Content course={course}/>
+      <Total course={course}/>
     </>
   )
 }
