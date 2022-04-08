4bb84dff00a5dcd08e5f89f52e46f9f0150e8157
Simon Clement <simon@simontc.com>
2022-04-08 11:27:28 +0300

    Part 2.b - phonebook step 3.5 - refactor

--- exercises/part2/phonebook/src/App.js
+++ exercises/part2/phonebook/src/App.js
@@ -1,8 +1,9 @@
 import { useState } from 'react'
+import * as PropTypes from "prop-types";
 
-const InputWithLabel = ({onChange, value, labelText}) => {
+const InputWithLabel = ({onChange: onNameChange, value, labelText}) => {
   return <div>
-    {labelText}: <input value={value} onChange={onChange}/>
+    {labelText}: <input value={value} onChange={onNameChange}/>
   </div>;
 }
 
@@ -14,9 +15,37 @@ const Person = ({person}) => {
   )
 }
 
+const NewEntry = ({newName, newPhoneNumber, onAddPerson, onNameChange, onPhoneNumberChange}) => {
+  return (
+    <div>
+      <h2>Add new person</h2>
+      <form>
+        <InputWithLabel labelText="name" value={newName} onChange={onNameChange}/>
+        <InputWithLabel labelText="number" value={newPhoneNumber} onChange={onPhoneNumberChange}/>
+        <div>
+          <button type="submit" onClick={onAddPerson}>add</button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+const PersonList = ({persons}) => {
+  return(
+    <div>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map(p =>
+          <Person key={p.name} person={p}/>
+        )}
+      </ul>
+    </div>
+  )
+}
+
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', phoneNumber: '040-1234567' }
+    {name: 'Arto Hellas', phoneNumber: '040-1234567'}
   ])
   const [newName, setNewName] = useState('')
   const [newPhoneNumber, setNewPhoneNumber] = useState('');
@@ -37,7 +66,7 @@ const App = () => {
     event.preventDefault()
     const newPerson = {name: newName, phoneNumber: newPhoneNumber}
 
-    if (persons.some(p => personsAreTheSame(p, newPerson ))){
+    if (persons.some(p => personsAreTheSame(p, newPerson))) {
       alert(`${newName} is already added to the phonebook`)
     } else {
       setPersons(persons.concat(newPerson))
@@ -48,20 +77,15 @@ const App = () => {
 
   return (
     <div>
-      <h2>Phonebook</h2>
-      <form>
-        <InputWithLabel labelText='name' value={newName} onChange={onNameChange}/>
-        <InputWithLabel labelText='number' value={newPhoneNumber} onChange={onPhoneNumberChange}/>
-        <div>
-          <button type="submit" onClick={onAddPerson}>add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      <ul>
-        {persons.map(p =>
-          <Person key={p.name} person={p}/>
-        )}
-      </ul>
+      <h1>Phonebook</h1>
+      <NewEntry
+        newName={newName}
+        onNameChange={onNameChange}
+        newPhoneNumber={newPhoneNumber}
+        onPhoneNumberChange={onPhoneNumberChange}
+        onAddPerson={onAddPerson}
+      />
+      <PersonList persons={persons}/>
     </div>
   )
 }
