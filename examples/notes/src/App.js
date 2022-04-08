394405a84d1b2ba6bbba266517601c33ab073ea9
Simon Clement <simon@simontc.com>
2022-04-08 10:34:16 +0300

    Part 2.b - Add form using a controlled component

--- examples/notes/src/App.js
+++ examples/notes/src/App.js
@@ -3,10 +3,24 @@ import Note from "./components/Note";
 
 const App = (props) => {
   const [notes, setNotes] = useState(props.notes);
+  const [newNote, setNewNote] = useState('');
+
+  const handleNoteChange = (event) => {
+    console.log(event.target.value)
+    setNewNote(event.target.value)
+  }
 
   const addNote = (event) => {
     event.preventDefault() // Prevents the default action on submitting a form. One thing it prevents is the reloading of the page.
-    console.log('button clicked', event.target)
+    const noteObject = {
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() < 0.5,
+      id: notes.length + 1,
+    }
+
+    setNotes(notes.concat(noteObject))
+    setNewNote('')
   }
 
   return (
@@ -18,7 +32,9 @@ const App = (props) => {
         )}
       </ul>
       <form onSubmit={addNote}>
-        <input/>
+        <input
+          value={newNote}
+          onChange={handleNoteChange}/>
         <button type="submit">save</button>
       </form>
     </div>
