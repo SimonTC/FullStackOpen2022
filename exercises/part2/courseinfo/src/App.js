6e6e5e6a1d7a923a97cd2e169cd75891b69ed43e
Simon Clement <simon@simontc.com>
2022-04-08 09:53:15 +0300

    part 2 - course info step 7 + 8 - Show sum of exercises

--- exercises/part2/courseinfo/src/App.js
+++ exercises/part2/courseinfo/src/App.js
@@ -1,6 +1,9 @@
 const Header = ({ course }) => <h1>{course}</h1>
 
-const Total = ({ sum }) => <p>Number of exercises {sum}</p>
+const Total = ({ parts }) => {
+  const sum = parts.map(part => part.exercises).reduce((total, i) => total + i )
+  return <p>Number of exercises {sum}</p>
+}
 
 const Part = ({ part }) =>
   <p>
@@ -17,6 +20,7 @@ const Course = ({course}) => {
     <div>
       <Header course={course.name} />
       <Content parts={course.parts} />
+      <Total parts={course.parts}/>
     </div>
   )
 }
