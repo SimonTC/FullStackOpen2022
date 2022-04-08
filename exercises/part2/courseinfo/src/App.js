33a14649b116e96086d46abaf4b956c96edef3a9
Simon Clement <simon@simontc.com>
2022-04-08 09:46:39 +0300

    part 2 - course info step 6 - Create single Course object

--- exercises/part2/courseinfo/src/App.js
+++ exercises/part2/courseinfo/src/App.js
@@ -9,41 +9,42 @@ const Part = ({ part }) =>
 
 const Content = ({ parts }) =>
   <>
-    <Part
-      part={parts[0]}
-    />
-    <Part
-      part={parts[1]}
-    />
-    <Part
-      part={parts[2]}
-    />
+    {parts.map(part => <Part key={part.id} part={part}/> )}
   </>
 
-const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
-
-  return (
+const Course = ({course}) => {
+  return(
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total sum={parts[0].exercises + parts[1].exercises + parts[2].exercises} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
     </div>
   )
+}
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        id: 1,
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        id: 2,
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        id: 3,
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return <Course course={course}/>
 }
 
 export default App
