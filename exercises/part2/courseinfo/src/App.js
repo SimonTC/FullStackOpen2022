b96f5bc153a72b358a9c87535a496fb8ae64432e
Simon Clement <simon@simontc.com>
2022-04-08 10:08:05 +0300

    part 2 - course info step 10 - Create separate file for Course component

--- exercises/part2/courseinfo/src/App.js
+++ exercises/part2/courseinfo/src/App.js
@@ -1,29 +1,4 @@
-const CourseHeader = ({ course }) => <h2>{course}</h2>
-
-const Total = ({ parts }) => {
-  const sum = parts.map(part => part.exercises).reduce((total, i) => total + i )
-  return <p>Total of {sum} exercises</p>
-}
-
-const Part = ({ part }) =>
-  <p>
-    {part.name} {part.exercises}
-  </p>
-
-const Content = ({ parts }) =>
-  <>
-    {parts.map(part => <Part key={part.id} part={part}/> )}
-  </>
-
-const Course = ({course}) => {
-  return(
-    <div>
-      <CourseHeader course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts}/>
-    </div>
-  )
-}
+import Course from "./components/Course";
 
 const App = () => {
   const courses = [
