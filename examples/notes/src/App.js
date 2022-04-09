32ad4a6a2f344a2528b491931974ec25028b276f
Simon Clement <simon@simontc.com>
2022-04-09 11:04:13 +0300

    Part 2.e - notes example - add timed error message on interacting with deleted note

--- examples/notes/src/App.js
+++ examples/notes/src/App.js
@@ -1,11 +1,14 @@
 import {useState, useEffect} from "react";
 import Note from "./components/Note";
+
 import noteService from './services/notes'
+import Notification from "./components/Notification";
 
 const App = () => {
   const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState('');
   const [showAll, setShowAll] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null);
 
   const toggleImportanceOf = (id) => {
     const note = notes.find(n => n.id === id)
@@ -17,9 +20,12 @@ const App = () => {
         setNotes(notes.map(note => note.id !== id ? note : returnedNote))
       })
       .catch(error => {
-        alert(
-          `the note ${note.content} was already deleted from server`
+        setErrorMessage(
+          `Note ${note.content} was already removed from server`
         )
+        setTimeout(() => {
+            setErrorMessage(null)
+          }, 5000)
         setNotes(notes.filter(n => n.id !== id))
       })
   }
@@ -59,6 +65,7 @@ const App = () => {
   return (
     <div>
       <h1>Notes</h1>
+      <Notification message={errorMessage}/>
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? 'important' : 'all'}
