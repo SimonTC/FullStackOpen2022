fbf5e71710c2df42d2703dbfd48342d283193e89
Simon Clement <simon@simontc.com>
2022-04-22 20:23:58 +0300

    Part 3.c - notes example - use mongo db for all POST and GET operations

--- examples/notes/backend/index.js
+++ examples/notes/backend/index.js
@@ -51,13 +51,10 @@ app.get('/api/notes', (request, response) => {
 })
 
 app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const note = notes.find(note => note.id === id)
-  if(note){
-    response.json(note)
-  } else{
-    response.status(404).end()
-  }
+  Note.findById(request.params.id)
+    .then(note => {
+      response.json(note)
+    })
 })
 
 app.delete('/api/notes/:id', (request, response) => {
@@ -66,13 +63,6 @@ app.delete('/api/notes/:id', (request, response) => {
 
   response.status(204).end()
 })
-
-const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => n.id))
-    : 0
-  return maxId + 1;
-}
 
 app.post('/api/notes', (request, response) => {
   const body = request.body
@@ -83,15 +73,16 @@ app.post('/api/notes', (request, response) => {
     })
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
     date: new Date(),
-    id: generateId()
-  }
+  })
 
-  notes = notes.concat(note)
-  response.json(note)
+  note.save()
+    .then(savedNote => {
+      response.json(savedNote)
+    })
 })
 
 const unknownEndpoint = (request, response) => {
