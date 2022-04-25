8f26ddafd3050864d243acaff345b909b28b943d
Simon Clement <simon@simontc.com>
2022-04-25 19:51:23 +0300

    Part 4.a - notes - add separate module for the middleware

---
+++ examples/notes/backend/utils/middleware.js
@@ -0,0 +1,31 @@
+const logger = require('./logger')
+
+const requestLogger = (request, response, next) => {
+  logger.info('Method:', request.method)
+  logger.info('Path:', request.path)
+  logger.info('Body:', request.body)
+  logger.info('---')
+  next() // Yields control to the next middleware
+}
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError'){
+    return response.status(400).send({ error: 'malformed id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+
+  next(error) // Send error on to the default express errorhandler
+}
+
+module.exports = {
+  requestLogger,
+  unknownEndpoint,
+  errorHandler
+}
