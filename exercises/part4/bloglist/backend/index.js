044c601d2578f5d7f39fab696ecb7503de1528f1
Simon Clement <simon@simontc.com>
2022-04-25 20:46:43 +0300

    Part 4.a - blog list - step 2 - extract blog controller

--- exercises/part4/bloglist/backend/index.js
+++ exercises/part4/bloglist/backend/index.js
@@ -5,30 +5,15 @@ const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 const Blog = require('./models/blog')
+const blogsRouter = require('./controllers/blogs')
 
 const mongoUrl = process.env.MONGODB_URI
 mongoose.connect(mongoUrl)
 
 app.use(cors())
 app.use(express.json())
+app.use('/api/blogs', blogsRouter)
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
