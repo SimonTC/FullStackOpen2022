1dbaba4473500f1642dd3ab9c7f7863e56777b3d
Simon Clement <simon@simontc.com>
2022-04-25 20:54:29 +0300

    Part 4.a - blog list - step 2 - extract app initialization

--- exercises/part4/bloglist/backend/index.js
+++ exercises/part4/bloglist/backend/index.js
@@ -1,19 +1,9 @@
-const config = require('./utils/config')
+const app = require('./app')
 const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-const blogsRouter = require('./controllers/blogs')
+const config = require('./utils/config')
 
-mongoose.connect(config.MONGODB_URI)
+const server = http.createServer(app)
 
-app.use(cors())
-app.use(express.json())
-app.use('/api/blogs', blogsRouter)
-
-
-const PORT = config.PORT
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+server.listen(config.PORT, () => {
+  console.log(`Server running on port ${config.PORT}`)
 })
