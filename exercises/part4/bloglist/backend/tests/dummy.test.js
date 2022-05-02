75324a07930c7afb8bf9c0fa4722e7b9a459d921
Simon Clement <simon@simontc.com>
2022-05-02 19:56:04 +0300

    Part 4.a - blog list - testing step 3 - Add method and tests for getting favorite blog

--- exercises/part4/bloglist/backend/tests/dummy.test.js
+++ exercises/part4/bloglist/backend/tests/dummy.test.js
@@ -1,3 +1,5 @@
+// noinspection PointlessArithmeticExpressionJS
+
 const listHelper = require('../utils/list_helper')
 const data = require('./test_data')
 
@@ -26,4 +28,36 @@ describe('total likes', () => {
     const result = listHelper.totalLikes(data.blogs)
     expect(result).toBe(expectedLikes)
   })
+})
+
+describe('favorite blog', () => {
+  test('of empty list is undefined', () => {
+    const blogs = []
+    const result = listHelper.favoriteBlog(blogs)
+    expect(result).toBe(undefined)
+  })
+
+  test('of single blog equals that blog', () => {
+    const blogs = [data.blogs[0]]
+    const result = listHelper.favoriteBlog(blogs)
+    const expectedFavorite =
+      {
+        title: blogs[0].title,
+        author: blogs[0].author,
+        likes: blogs[0].likes
+      }
+    expect(result).toEqual(expectedFavorite)
+  })
+
+  test('of many blogs equals the blog with the most likes', () => {
+    const result = listHelper.favoriteBlog(data.blogs)
+    const expectedFavorite =
+      {
+        title: 'Canonical string reduction',
+        author: 'Edsger W. Dijkstra',
+        likes: 12
+      }
+    expect(result).toEqual(expectedFavorite)
+  })
+
 })
--- exercises/part4/bloglist/backend/utils/list_helper.js
+++ exercises/part4/bloglist/backend/utils/list_helper.js
@@ -6,7 +6,30 @@ const totalLikes = (blogs) => {
   return blogs.reduce( (sum, blog) => sum + blog.likes, 0 )
 }
 
+const favoriteBlog = (blogs) => {
+  let favoriteBlog
+  let maxLikes = -1
+
+  for (const blog of blogs) {
+    if (blog.likes > maxLikes){
+      maxLikes = blog.likes
+      favoriteBlog = blog
+    }
+  }
+
+  if (favoriteBlog){
+    return {
+      title: favoriteBlog.title,
+      author: favoriteBlog.author,
+      likes: favoriteBlog.likes
+    }
+  }
+
+  return undefined
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
