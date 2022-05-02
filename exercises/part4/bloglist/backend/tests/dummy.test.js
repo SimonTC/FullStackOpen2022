25b824d6a3e861a5f1618425a560226381708afb
Simon Clement <simon@simontc.com>
2022-05-02 19:40:12 +0300

    Part 4.a - blog list - testing step 2 - Add method and tests for counting total likes

--- exercises/part4/bloglist/backend/tests/dummy.test.js
+++ exercises/part4/bloglist/backend/tests/dummy.test.js
@@ -1,8 +1,29 @@
 const listHelper = require('../utils/list_helper')
+const data = require('./test_data')
 
 test('dummy returns one', () => {
   const blogs = []
 
   const result = listHelper.dummy(blogs)
   expect(result).toBe(1)
+})
+
+describe('total likes', () => {
+  test('of empty list is zero', () => {
+    const blogs = []
+    const result = listHelper.totalLikes(blogs)
+    expect(result).toBe(0)
+  })
+
+  test('of single blog equals the likes of that blog', () => {
+    const blogs = [data.blogs[0]]
+    const result = listHelper.totalLikes(blogs)
+    expect(result).toBe(7)
+  })
+
+  test('of multiple blogs equals the sum of likes of all the blogs', () => {
+    const expectedLikes = 7 + 5 + 12 + 10 + 0 + 2 // = 36
+    const result = listHelper.totalLikes(data.blogs)
+    expect(result).toBe(expectedLikes)
+  })
 })
