76e2b1a7bae3dfbe15e1706aa5df9bf767507fc5
Simon Clement <simon@simontc.com>
2022-05-06 11:22:30 +0300

    Part 4.b - blogs - Check for correct process env when setting mongodb uri

--- exercises/part4/bloglist/backend/utils/config.js
+++ exercises/part4/bloglist/backend/utils/config.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 
-const MONGODB_URI = process.env.NODE_DEV === 'test'
+const MONGODB_URI = process.env.NODE_ENV === 'test'
   ? process.env.TEST_MONGODB_URI
   : process.env.MONGODB_URI
 const PORT = process.env.PORT
