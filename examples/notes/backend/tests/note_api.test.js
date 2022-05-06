a284d26256ccb5d4ee3d968b8ba4a08ce47c5d2a
Simon Clement <simon@simontc.com>
2022-05-06 10:09:54 +0300

    Part 4.b - notes - Fix wrong api call in test

--- examples/notes/backend/tests/note_api.test.js
+++ examples/notes/backend/tests/note_api.test.js
@@ -33,13 +33,13 @@ test('notes are returned as json', async () => {
 }, 10000)
 
 test('all notes are returned', async () => {
-  const response = await api.get('api/notes')
+  const response = await api.get('/api/notes')
 
   expect(response.body).toHaveLength(initialNotes.length)
 })
 
 test('a specific note is within the returned notes', async () => {
-  const response = await api.get('api/notes')
+  const response = await api.get('/api/notes')
 
   const contents = response.body.map(r => r.content)
   expect(contents).toContain(
