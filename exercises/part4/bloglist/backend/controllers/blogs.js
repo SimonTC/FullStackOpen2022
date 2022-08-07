8398e3799f26959f16bbc6b3292f075fc2241a6c
Simon Clement <simon@simontc.com>
2022-08-07 13:03:59 +0300

    Part 4.c - Blogs - Use user object from request when creating blog post

--- exercises/part4/bloglist/backend/controllers/blogs.js
+++ exercises/part4/bloglist/backend/controllers/blogs.js
@@ -1,7 +1,5 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
-const User = require('../models/user')
-const jwt = require('jsonwebtoken')
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog
@@ -12,12 +10,8 @@ blogsRouter.get('/', async (request, response) => {
 
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
-  const decodedToken = jwt.verify(request.token, process.env.SECRET)
-  if (!decodedToken.id){
-    return response.status(401).json({ error: 'token missing or invalid' })
-  }
+  const user = request.user
 
-  const user = await User.findById(decodedToken.id)
   const blog = new Blog({
     title: body.title,
     author: body.author,
