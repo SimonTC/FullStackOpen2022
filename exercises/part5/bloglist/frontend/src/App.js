40e891bcc3971023df043c4c1b4819d2fadd67ad
Simon Clement <simon@simontc.com>
2022-08-12 13:58:24 +0300

    Part 5.a - Blogs - Add ui for log in

--- exercises/part5/bloglist/frontend/src/App.js
+++ exercises/part5/bloglist/frontend/src/App.js
@@ -4,19 +4,63 @@ import blogService from './services/blogs'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [user, setUser] = useState(null);
 
-  useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
-  }, [])
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    console.log('logging in with', username, password)
+  }
 
-  return (
+  const loginForm = () => (
+    <div>
+      <h2>Log in to application</h2>
+      <form onSubmit={handleLogin}>
+        <div>
+          username
+          <input
+            type="text"
+            value={username}
+            name="Username"
+            onChange={({target}) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+          password
+          <input
+            type="password"
+            value={password}
+            name="Password"
+            onChange={({target}) => setPassword(target.value)}
+          />
+        </div>
+        <button type="submit">login</button>
+      </form>
+    </div>
+  )
+
+  const notesListAndEditing = () => (
     <div>
       <h2>blogs</h2>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
+    </div>
+  )
+
+  // useEffect(() => {
+  //   blogService.getAll().then(blogs =>
+  //     setBlogs( blogs )
+  //   )
+  // }, [])
+
+  return (
+    <div>
+      {user === null
+        ? loginForm()
+        : notesListAndEditing()
+      }
     </div>
   )
 }
