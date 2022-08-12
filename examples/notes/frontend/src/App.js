2d7c0056e1edc5051f164845db40b99760899196
Simon Clement <simon@simontc.com>
2022-08-12 13:24:25 +0300

    Part 5.a - Notes - Use user token when creating new notes

--- examples/notes/frontend/src/App.js
+++ examples/notes/frontend/src/App.js
@@ -63,6 +63,7 @@ const App = () => {
       const user = await loginService.login({
         username, password
       })
+      noteService.setToken(user.token)
       setUser(user)
       setUsername('')
       setPassword('')
