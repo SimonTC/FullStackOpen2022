c5721d06b6d7dc7984834c3d4ff505fd17fb731a
Simon Clement <simon@simontc.com>
2022-08-19 12:47:01 +0300

    Part 5.b - Blogs - Add support for showing blog details

--- exercises/part5/bloglist/frontend/src/components/Blog.js
+++ exercises/part5/bloglist/frontend/src/components/Blog.js
@@ -1,7 +1,34 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import {useState} from "react";
+
+const Blog = ({blog}) => {
+  const [showDetails, setShowDetails] = useState(false);
+
+  const toggleDetails = () => {
+    setShowDetails(!showDetails)
+  }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+    <div style={blogStyle}>
+      {blog.title}
+      <button onClick={toggleDetails}>{showDetails? 'hide' : 'view'}</button>
+      <div style={{ display: showDetails ? '' : 'none' }}>
+        {blog.url}
+        <br/>
+        Likes: {blog.likes}
+        <button>Like</button>
+        <br/>
+        {blog.author}
+      </div>
+    </div>
+  )
+}
 
 export default Blog
