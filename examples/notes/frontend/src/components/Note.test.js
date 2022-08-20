1fba4bd1994ab4afc0225d48c661e95019f99f1e
Simon Clement <simon@simontc.com>
2022-08-20 16:47:40 +0300

    Part 5.c - Notes - Add test for pressing a button

--- examples/notes/frontend/src/components/Note.test.js
+++ examples/notes/frontend/src/components/Note.test.js
@@ -2,6 +2,7 @@ import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
 import { render, screen } from '@testing-library/react'
 import Note from './Note'
+import userEvent from '@testing-library/user-event'
 
 test('renders content', () => {
   const noteContent = 'Component testing is done with react-testing library'
@@ -17,4 +18,22 @@ test('renders content', () => {
   screen.debug(element)
 
   expect(element).toBeDefined()
+})
+
+test('clicking the button calls event handler once',  async() => {
+  const noteContent = 'Component testing is done with react-testing library'
+  const note = {
+    content: noteContent,
+    important: true
+  }
+
+  const mockHandler = jest.fn()
+
+  render(<Note note={note} toggleImportance={mockHandler}/> )
+
+  const user = userEvent.setup()
+  const button = screen.getByText('make not important')
+  await user.click(button)
+
+  expect(mockHandler.mock.calls).toHaveLength(1)
 })
