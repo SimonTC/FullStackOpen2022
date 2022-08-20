d7f9f7401d57b868bb827e313688a7e6adeda15f
Simon Clement <simon@simontc.com>
2022-08-20 16:36:06 +0300

    Revert "Part 5.c - Notes - Use css selector in test"
    This reverts commit 6c76845c6edde62e102bb60c262b29298bd8d6ba.

--- examples/notes/frontend/src/components/Note.test.js
+++ examples/notes/frontend/src/components/Note.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import Note from './Note'
 
 test('renders content', () => {
@@ -10,8 +10,8 @@ test('renders content', () => {
     important: true
   }
 
-  const { container } = render(<Note note={note}/> )
+  render(<Note note={note}/> )
 
-  const div = container.querySelector('.note')
-  expect(div).toHaveTextContent(noteContent)
+  const element = screen.getByText(noteContent)
+  expect(element).toBeDefined()
 })
