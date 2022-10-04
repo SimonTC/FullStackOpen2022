c243c98ea7927f32b4b20d26be0ec7277386c035
Simon Clement <simon@simontc.com>
2022-10-04 20:26:34 +0300

    Part 5.d - Notes - Reset and create new data before running tests

--- examples/notes/frontend/cypress/e2e/note_app.cy.js
+++ examples/notes/frontend/cypress/e2e/note_app.cy.js
@@ -1,16 +1,19 @@
 describe('Note app', function () {
 
   beforeEach(function(){
+    cy.request('POST', 'http://localhost:3001/api/testing/reset')
+    const user = {
+      name: 'Matti Luukkainen',
+      username: 'mluukkai',
+      password: 'salainen'
+    }
+    cy.request('POST', 'http://localhost:3001/api/users/', user)
     cy.visit('http://localhost:3000')
   })
 
   it('front page can be opened', function() {
     cy.contains('Notes')
     cy.contains('Note app, Department of Computer Science, University of Helsinki 2022')
-  })
-
-  it('login form can be opened', function(){
-    cy.contains('Log in').click()
   })
 
   it('user can login', function () {
