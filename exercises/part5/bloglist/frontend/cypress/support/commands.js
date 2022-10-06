7a13dc54d6571c1506ad9026720258c18e89a0fc
Simon Clement <simon@simontc.com>
2022-10-06 19:58:53 +0300

    Part 5.d - Blogs - Add commands for log in and blog creation

--- exercises/part5/bloglist/frontend/cypress/support/commands.js
+++ exercises/part5/bloglist/frontend/cypress/support/commands.js
@@ -27,3 +27,25 @@
 Cypress.Commands.add('getBy', (cyAttr) => {
   cy.get(`[data-cy="${cyAttr}"]`)
 } )
+
+Cypress.Commands.add('login', ({ username, password }) => {
+  cy.request('POST', 'http://localhost:3003/api/login', {
+    username, password
+  }).then(({ body }) => {
+    localStorage.setItem('bloglistUser', JSON.stringify(body))
+    cy.visit('http://localhost:3000')
+  })
+})
+
+Cypress.Commands.add('createBlog', ({ title, author, url, likes }) => {
+  cy.request({
+    url: 'http://localhost:3003/api/blogs',
+    method: 'POST',
+    body: { title, author, url, likes },
+    headers: {
+      'Authorization': `bearer ${JSON.parse(localStorage.getItem('bloglistUser')).token}`
+    }
+  })
+
+  cy.visit('http://localhost:3000')
+})
