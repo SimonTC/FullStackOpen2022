aba965475ea5ee7fcdcad9714f8212e4160bf691
Simon Clement <simon@simontc.com>
2022-10-19 19:34:37 +0300

    Part 6.a - Notes - Make sure reducer method is pure

--- examples/notes-redux/src/reducers/noteReducer.js
+++ examples/notes-redux/src/reducers/noteReducer.js
@@ -1,8 +1,6 @@
 export const noteReducer = (state = [], action) => {
   if (action.type === 'NEW_NOTE'){
-    state.push(action.data)
-    return state
-    // return state.concat(action.data)
+    return state.concat(action.data)
   }
 
   return state
