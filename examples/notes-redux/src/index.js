c00a7156c1d4a7e02b46f2c4c94cd9ef8d262099
Simon Clement <simon@simontc.com>
2022-10-21 19:58:34 +0300

    Part 6.a - Notes - Make use of react-redux to split code into smaller components

--- examples/notes-redux/src/index.js
+++ examples/notes-redux/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 
 import {createStore} from "redux";
 import {noteReducer} from "./reducers/noteReducer";
+import {Provider} from "react-redux"
+import App from "./App";
 
 const store = createStore(noteReducer)
 
@@ -24,64 +26,8 @@ store.dispatch({
   }
 })
 
-const generateId = () =>
-  Number((Math.random() * 1000000).toFixed(0))
-
-const createNote = (content) => {
-  return {
-    type: 'NEW_NOTE',
-    data: {
-      content,
-      important: false,
-      id: generateId()
-    }
-  };
-}
-
-const toggleImportanceOf = (id) => {
-  return {
-    type: 'TOGGLE_IMPORTANCE',
-    data: { id }
-  };
-}
-
-const App = () => {
-  const addNote = (event) => {
-    event.preventDefault()
-    const content = event.target.note.value
-    event.target.note.value = ''
-    store.dispatch(createNote(content))
-  }
-
-  const toggleImportance = (id) => {
-    store.dispatch(toggleImportanceOf(id))
-  }
-
-  return(
-    <div>
-      <form onSubmit={addNote}>
-        <input name="note"/>
-        <button type="submit">add</button>
-      </form>
-      <ul>
-        {store.getState().map(note=>
-          <li
-            key={note.id}
-            onClick={() => toggleImportance(note.id)}
-          >
-            {note.content} <strong>{note.important ? 'important' : ''}</strong>
-          </li>
-        )}
-      </ul>
-    </div>
-  )
-}
-
-const root = ReactDOM.createRoot(document.getElementById('root'))
-const renderApp = () => root.render(<App />)
-
-// renderApp needs to be called once to do the initial rendering
-renderApp()
-
-// Without doing the render app subscription we would never render eanything on changes.
-store.subscribe(renderApp)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
+    <App/>
+  </Provider>
+)
