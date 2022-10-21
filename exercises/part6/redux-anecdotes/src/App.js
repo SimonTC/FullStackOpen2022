2b471b126dc34383a050810495361340c2be54ab
Simon Clement <simon@simontc.com>
2022-10-21 22:16:54 +0300

    Part 6.a - Anecdotes - Extract components - 2.5 h

--- exercises/part6/redux-anecdotes/src/App.js
+++ exercises/part6/redux-anecdotes/src/App.js
@@ -1,41 +1,13 @@
-import { useSelector, useDispatch } from 'react-redux'
-import {createAnecdote, increaseVoteFor} from "./reducers/anecdoteReducer";
+import AnecdoteList from "./components/Anecdotes";
+import AnecdoteForm from "./components/AnecdoteForm";
+
 
 const App = () => {
-  const anecdotes = useSelector(state => state)
-  const dispatch = useDispatch()
-
-  const vote = (id) => {
-    console.log('vote', id)
-    dispatch(increaseVoteFor(id))
-  }
-
-  const addAnecdote = (event) => {
-    event.preventDefault()
-    const anecdote = event.target.anecdote.value
-    event.target.anecdote.value = ''
-    dispatch(createAnecdote(anecdote))
-  }
-
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
-          </div>
-        </div>
-      )}
-      <h2>create new</h2>
-      <form onSubmit={addAnecdote}>
-        <input name="anecdote" />
-        <button type="submit">create</button>
-      </form>
+      <AnecdoteList/>
+      <AnecdoteForm/>
     </div>
   )
 }
---
+++ exercises/part6/redux-anecdotes/src/components/Anecdotes.js
@@ -0,0 +1,38 @@
+import {useDispatch, useSelector} from "react-redux";
+import {increaseVoteFor} from "../reducers/anecdoteReducer";
+
+const Anecdote = ({ anecdote, handleVoteClick }) => {
+  return (
+    <li>
+      <div>
+        {anecdote.content}
+      </div>
+      <div>
+        has {anecdote.votes}
+        <button onClick={handleVoteClick}>vote</button>
+      </div>
+    </li>
+  )
+}
+
+const AnecdoteList = () => {
+  const dispatch = useDispatch()
+  const anecdotes = useSelector(state => state)
+
+  const vote = (id) => {
+    dispatch(increaseVoteFor(id))
+  }
+
+  return (
+    <ul>
+      {anecdotes.map(anecdote =>
+        <Anecdote
+          key={anecdote.id}
+          anecdote={anecdote}
+          handleVoteClick={() => vote(anecdote.id)}/>
+      )}
+    </ul>
+  )
+}
+
+export default AnecdoteList
