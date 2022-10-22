1a927d675e061c13d073a0bc1a56f0c4b39760c2
Simon Clement <simon@simontc.com>
2022-10-22 17:05:03 +0300

    Part 6.c - Notes - Read data from db

--- examples/notes-redux/src/index.js
+++ examples/notes-redux/src/index.js
@@ -4,14 +4,21 @@ import ReactDOM from 'react-dom/client'
 import {Provider} from "react-redux"
 import App from "./App";
 import filterReducer from "./reducers/filterReducer";
-import noteReducer from "./reducers/noteReducer";
+import noteReducer, {appendNote} from "./reducers/noteReducer";
 import {configureStore} from "@reduxjs/toolkit";
+import noteService from './services/notes'
 
 const store = configureStore({
   reducer: {
     notes: noteReducer,
     filter: filterReducer
   }
+})
+
+noteService.getAll().then(notes => {
+  notes.forEach(note => {
+    store.dispatch(appendNote(note))
+  })
 })
 
 ReactDOM.createRoot(document.getElementById('root')).render(
