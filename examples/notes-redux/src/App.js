5a18dd34bae54dabfd6055a4bdb7fcea738e7fd3
Simon Clement <simon@simontc.com>
2022-10-22 17:24:02 +0300

    Part 6.c - Notes - Set notes in App using effect hook

--- examples/notes-redux/src/App.js
+++ examples/notes-redux/src/App.js
@@ -1,8 +1,18 @@
 import NewNote from "./components/NewNote";
 import Notes from "./components/Notes";
 import {VisibilityFilter} from "./components/VisibilityFilter";
+import {useDispatch} from "react-redux";
+import {useEffect} from "react";
+import noteService from './services/notes'
+import {setNotes} from "./reducers/noteReducer";
 
 export const App = () => {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    noteService.getAll().then(notes => dispatch(setNotes(notes)))
+  }, [dispatch]) // Dispatch actually never changes, but it is necessary to add it as a dependency to ensure linter is happy.
+
   return (
     <div>
       <NewNote/>
--- examples/notes-redux/src/index.js
+++ examples/notes-redux/src/index.js
@@ -4,19 +4,14 @@ import ReactDOM from 'react-dom/client'
 import {Provider} from "react-redux"
 import App from "./App";
 import filterReducer from "./reducers/filterReducer";
-import noteReducer, {appendNote, setNotes} from "./reducers/noteReducer";
+import noteReducer from "./reducers/noteReducer";
 import {configureStore} from "@reduxjs/toolkit";
-import noteService from './services/notes'
 
 const store = configureStore({
   reducer: {
     notes: noteReducer,
     filter: filterReducer
   }
-})
-
-noteService.getAll().then(notes => {
-  store.dispatch(setNotes(notes))
 })
 
 ReactDOM.createRoot(document.getElementById('root')).render(
