48424feddb0b1f87b4d991eb73b28ba37d6e685c
Simon Clement <simon@simontc.com>
2022-10-22 08:40:22 +0300

    Part 6.b - Notes - Create initial state

--- examples/notes-redux/src/index.js
+++ examples/notes-redux/src/index.js
@@ -8,24 +8,6 @@ import App from "./App";
 
 const store = createStore(noteReducer)
 
-store.dispatch({
-  type: 'NEW_NOTE',
-  data: {
-    content: 'the app state is in redux store',
-    important: true,
-    id: 1
-  }
-})
-
-store.dispatch({
-  type: 'NEW_NOTE',
-  data: {
-    content: 'state changes are made with actions',
-    important: false,
-    id: 2
-  }
-})
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <App/>
