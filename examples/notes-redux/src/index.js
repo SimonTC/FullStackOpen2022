0523db15d555355bdc6e6a8dba7d54cc765e53f3
Simon Clement <simon@simontc.com>
2022-10-22 17:07:47 +0300

    Part 6.c - Notes - Set notes directly instead of looping

--- examples/notes-redux/src/index.js
+++ examples/notes-redux/src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom/client'
 import {Provider} from "react-redux"
 import App from "./App";
 import filterReducer from "./reducers/filterReducer";
-import noteReducer, {appendNote} from "./reducers/noteReducer";
+import noteReducer, {appendNote, setNotes} from "./reducers/noteReducer";
 import {configureStore} from "@reduxjs/toolkit";
 import noteService from './services/notes'
 
@@ -16,9 +16,7 @@ const store = configureStore({
 })
 
 noteService.getAll().then(notes => {
-  notes.forEach(note => {
-    store.dispatch(appendNote(note))
-  })
+  store.dispatch(setNotes(notes))
 })
 
 ReactDOM.createRoot(document.getElementById('root')).render(
