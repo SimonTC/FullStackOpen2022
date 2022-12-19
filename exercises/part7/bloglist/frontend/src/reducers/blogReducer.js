e257cc744a8dbacd89387ea15bd1026e7bb942e1
Simon Clement <simon@simontc.com>
2022-12-19 19:44:22 +0200

    Part 7.f - Blog app - Add functions for blog state interaction

--- exercises/part7/bloglist/frontend/src/reducers/blogReducer.js
+++ exercises/part7/bloglist/frontend/src/reducers/blogReducer.js
@@ -1,5 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit';
 import blogService from '../services/blogs';
+import { setTimedNotification } from './notificationReducer';
 
 const blogSlice = createSlice({
   name: 'blogs',
@@ -11,6 +12,9 @@ const blogSlice = createSlice({
     appendBlog(state, action) {
       return [...state, action.payload];
     },
+    removeBlog(state, action) {
+      return state.filter((b) => b !== action.payload);
+    },
     updateBlog(state, action) {
       const changedBlog = action.payload;
       const id = changedBlog.id;
@@ -19,12 +23,41 @@ const blogSlice = createSlice({
   },
 });
 
-export const { setBlogs, appendBlog, updateBlog } = blogSlice.actions;
+export const { setBlogs, appendBlog, updateBlog, removeBlog } =
+  blogSlice.actions;
 
 export const getAllBlogs = () => {
   return async (dispatch) => {
     const blogs = await blogService.getAll();
     dispatch(setBlogs(blogs));
+  };
+};
+
+export const addBlogEntry = (blogObject) => {
+  return async (dispatch) => {
+    const createdBlog = await blogService.createNew(blogObject);
+    dispatch(appendBlog(createdBlog));
+
+    dispatch(
+      setTimedNotification({
+        message: `A new blog post titled "${createdBlog.title}" by ${createdBlog.author} added`,
+        isError: false,
+      })
+    );
+  };
+};
+
+export const deleteBlog = (blog) => {
+  return async (dispatch) => {
+    await blogService.deleteBlog(blog);
+    dispatch(removeBlog(blog));
+  };
+};
+
+export const addLikeFor = (blog) => {
+  return async (dispatch) => {
+    const updatedBlog = await blogService.update(blog);
+    dispatch(updateBlog(updatedBlog));
   };
 };
 
